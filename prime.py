26a60e01bd78540b4d9ce924ca9d41b51bd3d3da
Howard Lee <howardchlee@gmail.com>
2014-01-24 11:31:28 -0800

    Add python prime

---
+++ prime.py
@@ -0,0 +1,32 @@
+# some simple prime functions
+
+import math;
+
+def computePrimesUpTo(n):
+	ret = [];
+	isPrime = [True] * (n+1);
+	isPrime[0] = False;
+	isPrime[1] = False;
+	for i in range(n+1)[2:]:
+		if isPrime[i]:
+			ret.append[i];
+		for j in range(n+1)[i*i:n+1:i]:
+			isPrime[j] = False;
+	return ret
+	
+def getPrimeFactorsOf(n):
+	primePowers = {};
+		
+	i = 2;
+	while i <= n:
+		if(n % i == 0):
+			power = 0;
+			while(n % i == 0):
+				n/=i;
+				power+= 1;
+			primePowers[i] = power;
+		i+=1;
+	
+	return primePowers;
+
+		
